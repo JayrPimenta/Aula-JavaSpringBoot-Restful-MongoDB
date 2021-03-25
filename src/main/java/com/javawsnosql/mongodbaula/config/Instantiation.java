aece447d1211d1718962053e6c018ceb177f8fd0
Jayr Pimenta <jayr.pimenta@gmail.com>
2021-03-25 17:14:55 -0300

    Criado a entidade Post com entidade Usuario aninhado

--- src/main/java/com/javawsnosql/mongodbaula/config/Instantiation.java
+++ src/main/java/com/javawsnosql/mongodbaula/config/Instantiation.java
@@ -1,31 +1,47 @@
 package com.javawsnosql.mongodbaula.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.javawsnosql.mongodbaula.domain.Post;
 import com.javawsnosql.mongodbaula.domain.Usuario;
+import com.javawsnosql.mongodbaula.repositories.PostRepository;
 import com.javawsnosql.mongodbaula.repositories.UsuarioRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner{
 
 	@Autowired
+	private PostRepository postRepository;
+	
+	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		usuarioRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		Usuario alex = new Usuario(null, "Alex Carvalho", "alex-carvalho@gmail.com");
 		Usuario bianca = new Usuario(null, "Bianca Monteiro", "bia_monte@hotmail.com");
 		Usuario carla = new Usuario(null, "Carla Lopes Rodrigues", "carla.rog@outlook.com");
 		Usuario david = new Usuario(null, "David Santanna", "sant.david@uol.com.br");
 		
+		Post post1 = new Post(null, sdf.parse("23/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", bianca);
+		Post post2 = new Post(null, sdf.parse("11/09/2019"), "Bom dia", "Chalé e chocolate quente, dia feliz!", bianca);
+		Post post3 = new Post(null, sdf.parse("09/12/2019"), "Sol", "Hoje vai dar praia. Dia de pescaria ;)", carla);
+		
 		usuarioRepository.saveAll(Arrays.asList(alex, bianca, carla, david));
+		postRepository.saveAll(Arrays.asList(post1, post2, post3));
 	}
 
 }
---
+++ src/main/java/com/javawsnosql/mongodbaula/repositories/PostRepository.java
@@ -0,0 +1,10 @@
+package com.javawsnosql.mongodbaula.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.javawsnosql.mongodbaula.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
